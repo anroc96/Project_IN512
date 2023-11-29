fd4b89a6b009cc5b2a2887376de84c63e85c7b7f
anroc96 <anroc96@ulaval.ca>
2023-11-29 15:05:25 +0100

    GridBelieves Display

--- scripts/agent.py
+++ scripts/agent.py
@@ -9,6 +9,8 @@ from my_constants import *
 
 from threading import Thread
 import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 
 class Agent:
@@ -24,13 +26,11 @@ class Agent:
         self.x, self.y = env_conf["x"], env_conf["y"]   #initial agent position
         self.w, self.h = env_conf["w"], env_conf["h"]   #environment dimensions
         self.cell_val = env_conf["cell_val"] #value of the cell the agent is located in
+        Thread(target=self.msg_cb, daemon=True).start()
 
         #TODO: DEINE YOUR ATTRIBUTES HERE
         self.explo = np.zeros((env_conf["w"], env_conf["h"])) # Matrix of the explorated cells (0: not explorated, 1: explorated)
         self.believes = np.ones((env_conf["w"], env_conf["h"])) # Matrix of believes (values from 0 to 1)
-        self.heat_of_found_targets = np.zeros((env_conf["w"], env_conf["h"])) # Matrix of the heat of found targets to substract to cell_val
-
-        Thread(target=self.msg_cb, daemon=True).start()
 
 
     def msg_cb(self): 
@@ -38,24 +38,35 @@ class Agent:
         while self.running:
             msg = self.network.receive()
             print(msg)
-    
-    
+            # Update values
+            if msg["sender"] == -1:
+                self.x, self.y = msg["x"], msg["y"]   #update agent position
+                self.cell_val = msg["cell_val"] #value of the cell the agent is located in
+
 
     #TODO: CREATE YOUR METHODS HERE...
 
     def explore_cell(self):
-        self.explo[self.x, self.y] = 1 # Marking the cell as explored
-
+        # Marking the cell as explored
+        self.explo[self.x, self.y] = 1 
         # Updating believes with cell probability
-        self.believes[self.x, self.y] = self.cell_val - self.heat_of_found_targets[self.x, self.y]
-
-        # Todo: update belives in cells close to the position of the robot
-        # 1) Explore the map until a heat signature is found
-        # 2) Update believes using method in the last slides of the PPT and move towards target
-        # 3) Reset believes
-        # 3) Once target is found, substract heat of this target from cell_value
+        self.believes[self.x, self.y] = self.cell_val
     
-
+    def plot_believes(self):
+        plt.figure(self.agent_id+1, figsize=(6,6.5))
+        # Creating colormap with explored cells
+        plt.pcolormesh(np.flip(self.explo.T, 0), cmap='Blues', edgecolors='k', vmin=0, vmax=2)
+        # Adding believes as annotations on every cell
+        for i in range(self.h):
+            for j in range(self.w):
+                plt.annotate(str(np.flip(self.believes.T, 0)[j][i]), xy=(i+0.5, j+0.5), ha='center', va='center', color='black')
+        plt.axis('equal')
+        plt.axis('off')
+        plt.title(f'GridBelieves for robot {self.agent_id+1}')
+        # Add total number of visited cells
+        plt.annotate(f'Number of visited cells: {int(self.explo.sum())}', xy=(0,-1), color='black', annotation_clip=False)
+        plt.tight_layout()
+        plt.show()
 
 
 
@@ -70,6 +81,8 @@ if __name__ == "__main__":
     agent = Agent(args.server_ip)
     try:    #Manual control test
         while True:
+            agent.explore_cell()
+            agent.plot_believes()
             cmds = {"header": int(input("0 <-> Broadcast msg\n1 <-> Get data\n2 <-> Move\n3 <-> Get nb connected agents\n4 <-> Get nb agents\n5 <-> Get item owner\n"))}
             if cmds["header"] == BROADCAST_MSG:
                 cmds["Msg type"] = int(input("1 <-> Key discovered\n2 <-> Box discovered\n3 <-> Completed\n"))
@@ -78,6 +91,8 @@ if __name__ == "__main__":
             elif cmds["header"] == MOVE:
                 cmds["direction"] = int(input("0 <-> Stand\n1 <-> Left\n2 <-> Right\n3 <-> Up\n4 <-> Down\n5 <-> UL\n6 <-> UR\n7 <-> DL\n8 <-> DR\n"))
             agent.network.send(cmds)
+            time.sleep(0.01) # Added time sleep to allow for receiving incoming message in other thread before next iteration
+
     except KeyboardInterrupt:
         pass
 
